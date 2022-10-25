4bb3cfdd0825fb08a917b309bda0e0e2e46161e0
mahekupadhye31 <114089022+mahekupadhye31@users.noreply.github.com>
2022-10-25 19:15:56 +0530

    Update todofinal.js

--- unicode_task1/todofinal.js
+++ unicode_task1/todofinal.js
@@ -1,74 +1,75 @@
-async function display_todo_list(){
-    let response= await fetch("https://jsonplaceholder.typicode.com/todos");
-    response= await response.json();
-    console.log(response);
-
-
-    let tableData="";
-    response.forEach(stats);
-    function stats(values){
-        tableData+= `
-        <tr id="tabledata">
-        <td>${values.id}</td>
-        <td id="tabletitle">${values.title}</td>
-        <td>${values.completed}</td>
-        <!-- <td><button class="btn btn-outline-info" type="button" name="button" data-bs-toggle="modal" data-bs-target="EditModal">Edit</button></td>  -->
-        <td>
-         <button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#example1Modal">
-          Edit
-        </button>
-        <!-- Modal -->
-          <div class="modal fade" id="example1Modal" tabindex="-1" aria-labelledby="example1ModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="example1ModalLabel">Edit your title</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <input id="edittaskinput" class="form-control" type="text" placeholder="Enter title here.." aria-label="default input example">
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary" id="editbtn" onclick="edit()">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </td>
-        <td>
-          <button id="todo" type="button"  class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#example2Modal">
-            Delete
-          </button>
-          <!-- Modal -->
-            <div class="modal fade" id="example2Modal" tabindex="-1" aria-labelledby="example2ModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="example2ModalLabel">Delete:</h1>
-                    <button  type="button" class="btn-close"  data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    Are you sure you want to delete your Title?
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button   type="button"  method="DELETE" class="btn btn-primary" onclick="remove()" data-bs-dismiss="modal">Delete</button>
+function getuser(){
+    var useid = localStorage.getItem("userid");
+    console.log(useid);
+    let dis="";
+    dis=`<h4> Welcome User ${useid}</h4>`;
+    document.getElementById("welcom").innerHTML=dis;
+    fetch(`https://jsonplaceholder.typicode.com/users/${useid}/todos`)
+    .then((data1)=>{
+        console.log(data1)
+        return data1.json();
+      })
+    .then((objectdata1)=>{
+        let todo="";
+        objectdata1.map((values1)=>{
+            todo+=`<tr id="todoitem">
+            <th scope="row">${values1.id}</th>
+            <td id="tabletitle">${values1.title}</td>
+            <td>${values1.completed}</td>           
+            <td>
+              <button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#example1Modal">
+              Edit
+              </button>
+              <!-- Modal -->
+              <div class="modal fade" id="example1Modal" tabindex="-1" aria-labelledby="example1ModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="example1ModalLabel">Edit your title</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <input id="edittaskinput" class="form-control" type="text" placeholder="Enter title here.." aria-label="default input example">
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary" id="editbtn" onclick="edit()">Save changes</button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-        </td>
-    </tr> `;
-
-    }
-    document.getElementById("tododata").innerHTML=tableData;
-    return response;
+            </td>
+            <td>
+              <button id="todo" type="button"  class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#example2Modal">
+                Delete
+              </button>
+              <!-- Modal -->
+                <div class="modal fade" id="example2Modal" tabindex="-1" aria-labelledby="example2ModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="example2ModalLabel">Delete:</h1>
+                        <button  type="button" class="btn-close"  data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        Are you sure you want to delete your Title?
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" method="DELETE" class="btn btn-primary" onClick="remove()" data-bs-dismiss="modal">Delete</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+             </td>
+            
+          </tr>`
+        })
+        document.getElementById("tododata").innerHTML=todo;
+    })
 }
-display_todo_list();
-
 function remove(){
-  var elem= document.getElementById('tabledata');
+  var elem= document.getElementById('todoitem');
   elem.parentNode.removeChild(elem);
   return false;
 }
@@ -79,6 +80,3 @@ function edit(){
   var showtitle=document.getElementById("tabletitle");
   showtitle.innerHTML=tit;
 }
-
-
-
